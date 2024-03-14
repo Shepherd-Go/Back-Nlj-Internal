be34fc1b303e2a1794a591f573d573aa68fcb102
Shepherd <neiferjr15@gmail.com>
2024-03-13 22:37:44 -0400

    feat: agg echo framework

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/BBCompanyca/Back-Nlj-Internal.git/providers"
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("Hello Word!!")
+
+	container := providers.BuildContainer()
+
+	if err := container.Invoke(func(server *echo.Echo) {
+		server.Logger.Fatal(server.Start(fmt.Sprintf(":%d", 3000)))
+	}); err != nil {
+		log.Fatal(err)
+	}
+
 }
---
+++ providers/di.go
@@ -0,0 +1,19 @@
+package providers
+
+import (
+	"github.com/labstack/echo/v4"
+	"go.uber.org/dig"
+)
+
+var Container *dig.Container
+
+func BuildContainer() *dig.Container {
+
+	Container = dig.New()
+
+	_ = Container.Provide(func() *echo.Echo {
+		return echo.New()
+	})
+
+	return Container
+}
