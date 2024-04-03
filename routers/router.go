1deb9a23b4ae29d72df76af2e3339cc59eb7349e
Shepherd <neiferjr15@gmail.com>
2024-04-02 22:39:27 -0400

    feat: :sparkles: Added functionality to add login cookie

--- routers/router.go
+++ routers/router.go
@@ -29,8 +29,10 @@ func (rtr *Router) Init() {
 	}))
 
 	rtr.server.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*"},
-		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+		AllowOrigins:     []string{"http://127.0.0.1:5500"},
+		AllowMethods:     []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+		AllowHeaders:     []string{echo.HeaderContentType},
+		AllowCredentials: true,
 	}))
 
 	rtr.server.Use(middleware.Recover())
